208b5769a8cc54ac15a593a9be5cf993c94e4c6c
Ethan Levien <ethanlevien@gmail.com>
2020-11-13 15:52:58 -0500

    test

--- branching_process.js
+++
@@ -1,9 +0,0 @@
-const height = 200;
-const width = 500;
-
-
-margin = ({top: 20, right: 3, bottom: 20, left: 3});
-
-line = d3.line().curve(d3.curveStepAfter)
-    .x(d => x(d.y))
-    .y(d => y(d.x))
